3bf3723f241c1b53bc6e23f7da984c0d3e777d18
Fabiola De La Torre <cubias.fabiola@gmail.com>
2023-10-10 01:49:10 -0500

    lab complete

--- main.js
+++ main.js
@@ -3,10 +3,28 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const heart = document.querySelector('.like-glyph')
+const errorModal = document.getElementById('modal');
+const errorMessageElement = document.getElementById('modal-message');
+function likeClick() {
+  heart.addEventListener('click', () => {
+    mimicServerCall()
+      .then( () => {
+        heart.textContent = FULL_HEART;
+        heart.classList.add('activated-heart');
+      })
+      .catch((error) => {
+        errorMessageElement.textContent = error;
+        errorModal.classList.remove('hidden'); // Display error modal
+        setTimeout(() => {
+          errorModal.classList.add('hidden'); // Hide error modal after 3 seconds
+        }, 3000);
+      });   
+    });
+  };
 
 
-
-
+        
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
